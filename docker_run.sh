c33f3957748c5c691f434caf7ca86210cfa9620c
Danaipat Sodkomkham <jo@macbook-air.local>
2019-06-30 14:11:15 +0900

    fix jupyter

--- docker_run.sh
+++ docker_run.sh
@@ -5,6 +5,8 @@ docker run $runtime -it \
            --rm \
            --name jupyterlab-tf \
            -u $(id -u):$(id -g) \
+           -v ${HOME}/.local:/.local \
+           -v ${HOME}/.jupyter:/.jupyter \
            -v ${HOME}/notebooks:/work_dir/notebooks \
            -v /mnt/data:/work_dir/data \
            -v /mnt/work_ssd:/work_dir/ssd \
