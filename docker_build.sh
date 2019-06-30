f60f4a1f8464a0945c9b1ac011db91529d9fe05f
Danaipat Sodkomkham <jo@macbook-air.local>
2019-06-30 11:03:47 +0900

    add git tag build

--- docker_build.sh
+++ docker_build.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-VERSION=1.0
-
 TF_DOCKER_TAG="latest-py3 latest-gpu-py3 nightly-py3 nightly-gpu-py3"
 
 function build () {
@@ -11,6 +9,9 @@ function build () {
                --tag jupyterlab-tf: \
                .
 }
+
+
+git fetch --all --tags --prune
 
 for tag in $TF_DOCKER_TAG;
 do
