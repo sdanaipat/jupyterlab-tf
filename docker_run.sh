0bb8f46078534f0b6efced20f297ba78cef3c241
Danaipat Sodkomkham <jo@macbook-air.local>
2019-06-30 12:39:16 +0900

    update run script

--- docker_run.sh
+++ docker_run.sh
@@ -1,9 +1,9 @@
-nvidia-docker run -it \
-                  --rm \
-                  --name tensorflow-container
-                  -u $(id -u):$(id -g) \
-                  -v ${HOME}/notebooks:/tf/notebooks \
-                  -v /mnt/data:/tf/data \
-                  -v /mnt/work_ssd:/tf/ssd \
-                  -p 8888:8888 \
-                  tensorflow
+docker run -it \
+           --rm \
+           --name jupyterlab-tf
+           -u $(id -u):$(id -g) \
+           -v ${HOME}/notebooks:/work_dir/notebooks \
+           -v /mnt/data:/work_dir/data \
+           -v /mnt/work_ssd:/work_dir/ssd \
+           -p 8888:8888 \
+           sdanaipat/jupyter-tf
