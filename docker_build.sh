0badbe4fd20ddf521c9393c3c7b5d6c1b4f72d0a
Danaipat Sodkomkham <jo@macbook-air.local>
2019-06-30 12:07:52 +0900

    add build script

--- docker_build.sh
+++ docker_build.sh
@@ -1,20 +1,29 @@
 #!/usr/bin/env bash
 
-TF_DOCKER_TAG="latest-py3 latest-gpu-py3 nightly-py3 nightly-gpu-py3"
+TF_DOCKER_TAGS="latest-py3 latest-gpu-py3 nightly-py3 nightly-gpu-py3"
 
 function build () {
-  tag=$1
-  echo $tag
-  docker build --build-arg tf_docker_tag=$tag \
-               --tag jupyterlab-tf: \
+  git_tag=$1
+  tf_tag=$2
+  docker build --build-arg tf_docker_tag=$tf_tag \
+               --tag jupyterlab-tf:$git_tag-$tf_tag \
                .
 }
 
-
 git fetch --all --tags --prune
+current_branch=$(git branch | grep \* | cut -d ' ' -f2)
 
-for tag in $TF_DOCKER_TAG;
+git stash
+GIT_TAGS="master $(git tag)"
+for git_tag in $GIT_TAGS;
 do
-  echo "building tag: $tag"
-  build $tag
+  git checkout git_tag
+  for tf_tag in $TF_DOCKER_TAGS;
+  do
+    echo "building git-tag: $git_tag, tf-tag: $tf_tag"
+    build $git_tag $tf_tag
+  done
 done
+
+git checkout $current_branch
+git stash pop
