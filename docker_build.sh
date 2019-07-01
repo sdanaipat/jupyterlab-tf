fa694f9a80905f227dc13751bd17cd6a3201bce2
Danaipat Sodkomkham <jo@macbook-air.local>
2019-07-01 22:09:23 +0900

    fix build tag

--- docker_build.sh
+++ docker_build.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-TF_DOCKER_TAGS="nightly-gpu-py3"
+TF_DOCKER_TAGS="latest-gpu-py3 nightly-gpu-py3"
 
 function build () {
   tf_tag=$1
   docker build --build-arg tf_docker_tag=$tf_tag \
-               --tag sdanaipat/jupyterlab-tf:latest-$tf_tag \
+               --tag sdanaipat/jupyterlab-tf:dev-$tf_tag \
                . && \
-  docker push sdanaipat/jupyterlab-tf:latest-$tf_tag
+  docker push sdanaipat/jupyterlab-tf:dev-$tf_tag
 }
 
 for tf_tag in $TF_DOCKER_TAGS;
