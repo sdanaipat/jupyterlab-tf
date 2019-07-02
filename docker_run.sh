d7821717899e6274ac89bd1d384c2dd231511b67
Danaipat Sodkomkham <jo@macbook-air.local>
2019-07-02 21:46:32 +0900

    mount local .keras

--- docker_run.sh
+++ docker_run.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 runtime=$1
 tag=$2
+
+mkdir -p ${HOME}/.keras
+mkdir -p ${HOME}/.jupyter
+
 docker run $runtime -it \
            --rm \
            --name jupyterlab-tf \
            -u $(id -u):$(id -g) \
+           -v ${HOME}/.keras:/work_dir/.keras \
            -v ${HOME}/.jupyter:/work_dir/.jupyter \
            -v ${HOME}/notebooks:/work_dir/notebooks \
            -v /mnt/data:/work_dir/data \
